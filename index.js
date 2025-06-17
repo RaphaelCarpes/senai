a776a346981980208ff49e5768cc85cd2974a2d6
RaphaelCarpes <raphaelg.carpes@gmail.com>
2025-06-17 17:11:24 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const botaoiniciar = document.getElementById("diviniciar")
+const divareajogo = document.getElementById("areajogo")
+
+const perguntas = [
+    "Qual é o maior deserto do mundo?",
+     "Qual é a capital do Brasil?",
+     "Qual é a capital da Austrália?",
+     "Qual é o país com maior população no mundo?",
+     " Qual a linha imaginária que atravessa o Brasil?",
+     "Qual o oceano que banha o Brasil?",
+]
+
+let botaodepergunta = document.createElement("button")
+let indiceperguntas = 0
+/* Funções */
+function iniciarjogo(){
+    //fecharbotaoinicio()
+    abrirareajogo()
+}
+
+function fecharbotaoinicio(){
+    botaoiniciar.innerHTML = ""
+}
+
+function abrirareajogo(){
+    areajogo.classList.add("active")
+
+    let botaodepergunta = document.createElement("button")
+    botaodepergunta.textContent = perguntas[indiceperguntas]
+    botaodepergunta.classList.add("answer-btn")
+    areaperguntas.appendChild(botaodepergunta)
+}
+
+function proximapergunta(){
+    indiceperguntas ++
+    if(indiceperguntas < perguntas.length)
+         abrirareajogo()
+    else
+    alert("Nao existe mais perguntas")
+}
